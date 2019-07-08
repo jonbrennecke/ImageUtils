0e8211052541478247f3bc3ad2cfdeba96fc119f
Jon Brennecke <jpbrennecke@gmail.com>
2019-07-07 18:14:03 -0700

    moved resize function into HSImageBuffer

--- HSCameraUtils/source/HSImageBuffer.swift
+++ HSCameraUtils/source/HSImageBuffer.swift
@@ -2,7 +2,7 @@ import AVFoundation
 import Accelerate
 
 public struct HSImageBuffer {
-  private let pixelBuffer: HSPixelBuffer
+  public let pixelBuffer: HSPixelBuffer
 
   public var size: Size<Int> {
     return pixelBuffer.size
@@ -10,6 +10,10 @@ public struct HSImageBuffer {
 
   public init(pixelBuffer: HSPixelBuffer) {
     self.pixelBuffer = pixelBuffer
+  }
+  
+  public init(cvPixelBuffer buffer: CVPixelBuffer) {
+    self.pixelBuffer = HSPixelBuffer(pixelBuffer: buffer)
   }
   
   public func makeVImageBuffer() -> vImage_Buffer {
@@ -48,5 +52,84 @@ public struct HSImageBuffer {
       return nil
     }
     return image?.takeRetainedValue()
+  }
+  
+  public func resize(
+    to outputSize: Size<Int>,
+    pixelBufferPool: CVPixelBufferPool,
+    isGrayscale: Bool = false
+  ) -> HSImageBuffer? {
+    let bufferInfo = pixelBuffer.bufferInfo
+    var srcBuffer = makeVImageBuffer()
+    
+    // create an empty destination vImage_Buffer
+    let destHeight = vImagePixelCount(outputSize.height)
+    let destWidth = vImagePixelCount(outputSize.width)
+    let destTotalBytes = outputSize.height * outputSize.width * bufferInfo.bytesPerPixel
+    let destBytesPerRow = outputSize.width * bufferInfo.bytesPerPixel
+    guard let destData = malloc(destTotalBytes) else {
+      return nil
+    }
+    var destBuffer = vImage_Buffer(
+      data: destData,
+      height: destHeight,
+      width: destWidth,
+      rowBytes: destBytesPerRow
+    )
+    
+    // scale
+    let resizeFlags = vImage_Flags(kvImageHighQualityResampling)
+    if isGrayscale {
+      let error = vImageScale_Planar8(&srcBuffer, &destBuffer, nil, resizeFlags)
+      if error != kvImageNoError {
+        free(destData)
+        return nil
+      }
+    } else {
+      let error = vImageScale_ARGB8888(&srcBuffer, &destBuffer, nil, resizeFlags)
+      if error != kvImageNoError {
+        free(destData)
+        return nil
+      }
+    }
+    
+    guard let destPixelBuffer = createPixelBuffer(with: pixelBufferPool) else {
+      free(destData)
+      return nil
+    }
+    
+    // save vImageBuffer to CVPixelBuffer
+    
+    var cgImageFormat = vImage_CGImageFormat(
+      bitsPerComponent: UInt32(bufferInfo.bitsPerComponent),
+      bitsPerPixel: UInt32(bufferInfo.bitsPerPixel),
+      colorSpace: Unmanaged.passRetained(bufferInfo.colorSpace),
+      bitmapInfo: bufferInfo.bitmapInfo,
+      version: 0,
+      decode: nil,
+      renderingIntent: .defaultIntent
+    )
+    
+    guard let cvImageFormat = vImageCVImageFormat_CreateWithCVPixelBuffer(destPixelBuffer)?.takeRetainedValue() else {
+      free(destData)
+      return nil
+    }
+    vImageCVImageFormat_SetColorSpace(cvImageFormat, bufferInfo.colorSpace)
+    
+    let copyError = vImageBuffer_CopyToCVPixelBuffer(
+      &destBuffer,
+      &cgImageFormat,
+      destPixelBuffer,
+      cvImageFormat,
+      nil,
+      vImage_Flags(kvImageNoFlags)
+    )
+    
+    if copyError != kvImageNoError {
+      free(destData)
+      return nil
+    }
+    free(destData)
+    return HSImageBuffer(cvPixelBuffer: destPixelBuffer)
   }
 }
