91b9e41eadb1eb2f1b2aa2b5fc908af2e7afdf6a
Jon Brennecke <jpbrennecke@gmail.com>
2020-04-21 12:59:30 -0700

    added ios static lib target

---
+++ ImageUtils/ImageUtils.swift
@@ -0,0 +1,11 @@
+//
+//  ImageUtils.swift
+//  ImageUtils
+//
+//  Created by Jonathan Brennecke on 4/21/20.
+//  Copyright © 2020 Jonathan Brennecke. All rights reserved.
+//
+
+class ImageUtils {
+
+}
